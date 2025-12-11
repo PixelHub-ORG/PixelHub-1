99688c095e8ab3f3ababc1370f166b53aa41447e
JoseLu2121 <josmorver@alum.us.es>
2025-12-11 23:57:58 +0100

    fix: zenodo selenium test fixed

--- app/modules/cart/tests/locustfile.py
+++ app/modules/cart/tests/locustfile.py
@@ -1,21 +1,17 @@
 # app/modules/cart/tests/locustfile.py
-from locust import HttpUser, task, between
-from core.locust.common import get_csrf_token
+from locust import HttpUser, between, task
 from core.environment.host import get_host_for_locust_testing
 
 
 class CartUser(HttpUser):
     wait_time = between(1, 5)
-    
+
     host = get_host_for_locust_testing()
 
     def on_start(self):
         # 1. Loguearse al iniciar
-        response = self.client.post("/login", data={
-            "email": "user1@example.com", 
-            "password": "1234"
-        })
-        
+        # response = self.client.post("/login", data={"email": "user1@example.com", "password": "1234"})
+
         # 2. Asegurar que el carro tiene algo (Añadimos el feature model con ID 1)
         self.client.post("/featuremodel/cart/add", json={"item_id": 1})
 
@@ -31,6 +27,6 @@ class CartUser(HttpUser):
                 response.success()
             elif response.status_code == 400 and "empty" in response.text:
                 # Si falla porque está vacío (raro porque lo llenamos en on_start), no lo marcamos como error crítico
-                response.success() 
+                response.success()
             else:
                 response.failure(f"Fallo al descargar: {response.status_code}")
