a19ce422402135388f99ae7cff74eeb737089c56
Jesus Sanchez Quiros <jesus04122@gmail.com>
2025-12-12 18:51:18 +0100

    chore: formateo automático de código (autopep8, black, isort)

--- app/modules/hubfile/forms.py
+++ app/modules/hubfile/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 
-#hola
-v = 3 +       323
+# hola
+v = 3 + 323
+
+
 class HubfileForm(FlaskForm):
     submit = SubmitField("Save hubfile")
